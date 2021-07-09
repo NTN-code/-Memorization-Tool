69c9434176b7308d57a5c751a9fdf3936204c8a3
Anton <tk2007m@mail.ru>
2021-07-10 00:52:25 +0300

    replace

--- tool.py
+++ tool.py
@@ -4,10 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-if os.path.exists('flashcard.db'):
-    os.remove('flashcard.db')
+# if os.path.exists('flashcard.db'):
+#     os.remove('flashcard.db')
 
-engine = create_engine('sqlite:///flashcard.db', echo=False)
+engine = create_engine('sqlite:///flashcard.db',  echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -18,6 +18,7 @@ class Flashcard(Base):
     question_id = Column(Integer, primary_key=True)
     question = Column(String)
     answer = Column(String)
+    box_number = Column(Integer)
 
     def __repr__(self):
         return f"Flashcard {self.question_id}"
@@ -36,13 +37,12 @@ def add_flash_card():
             while flag_add_flash_card_answer:
                 answer = input('Answer:\n')
                 if len(answer) != 0:
-                    flash_card = Flashcard(question=question, answer=answer)
+                    flash_card = Flashcard(question=question, answer=answer, box_number=1)
                     session.add(flash_card)
                     session.commit()
                     flag_add_flash_card_answer = False
         else:
             pass
-
 
 
 def menu_add_flash_card():
@@ -63,26 +63,90 @@ def get_all_card():
     return flash_cards
 
 
-def count_flash_card():
-    count_flash_card = session.query(Flashcard).count()
-    return count_flash_card
+def menu_update_delete(flash_card):
+    print("""press "d" to delete the flashcard:
+press "e" to edit the flashcard:""")
+    flag_menu_update_delete = True
+    while flag_menu_update_delete:
+        user_choice = input()
+        if user_choice == 'd':
+            delete_flash_card(flash_card)
+            flag_menu_update_delete = False
+        elif user_choice == 'e':
+            update_flash_card(flash_card)
+            flag_menu_update_delete = False
+        else:
+            print(f'{user_choice} is not an option')
+
+
+def update_flash_card(flash_card):
+    print(f"current question: {flash_card.question}")
+    print("please write a new question:")
+    new_question = input()
+    if new_question != ' ':
+        flash_card.question = new_question
+    print(f"current answer: {flash_card.answer}")
+    print("please write a new answer:")
+    new_answer = input()
+    if new_answer != ' ':
+        flash_card.answer = new_answer
+    session.commit()
+    
+
+def delete_flash_card(flash_card):
+    session.delete(flash_card)
+    session.commit()
+
+
+def menu_user_choice_correct(flash_card):
+    flag_menu_user_user_correct = True
+    while flag_menu_user_user_correct:
+        print('press "y" if your answer is correct:')
+        print('press "n" if your answer is wrong:')
+        user_correct = input()
+        if user_correct == "y":
+            flash_card.box_number += 1
+            flag_menu_user_user_correct = False
+            session.commit()
+        elif user_correct == "n":
+            if flash_card.box_number != 1:
+                flash_card.box_number -= 1
+                session.commit()
+            flag_menu_user_user_correct = False
+        else:
+            print(f'{user_correct} is not an option')
+    if flash_card.box_number == 4:
+        session.delete(flash_card)
+        session.commit()
 
 
 def menu_practice_flash_card():
     query = get_all_card()
-    count_flash_cards = count_flash_card()
     if query is not None:
-        questions = [query[x].question for x in range(count_flash_cards)]
-        answers = [query[x].answer for x in range(count_flash_cards)]
-        flash_cards = [zip(questions, answers)]
-        for flash_card in flash_cards:
-            for question, answer in flash_card:
-                user_choice = input(f"""Question: {question}
-Please press "y" to see the answer or press "n" to skip: """)
-                if user_choice == 'y':
-                    print(f"Answer: {answer}")
-                else:
-                    pass
+        for flash_card in query:
+            if flash_card.box_number != 4:
+                question = flash_card.question
+                answer = flash_card.answer
+                flag_menu_practice_flash_card = True
+                while flag_menu_practice_flash_card:
+                    print(f"Question: {question}")
+                    print('press "y" to see the answer:')
+                    print('press "n" to skip:')
+                    print('press "u" to update:')
+                    user_choice = input()
+                    if user_choice == 'y':
+                        print(f"Answer: {answer}")
+                        flag_menu_practice_flash_card = False
+                        menu_user_choice_correct(flash_card)
+                    elif user_choice == 'n':
+                        flag_menu_practice_flash_card = False
+                        menu_user_choice_correct(flash_card)
+                        pass
+                    elif user_choice == 'u':
+                        menu_update_delete(flash_card)
+                        flag_menu_practice_flash_card = False
+                    else:
+                        print(f'{user_choice} is not an option')
     else:
         print('There is no flashcard to practice!')
 
@@ -96,7 +160,10 @@ def menu_main():
         if user_choice == '1':
             menu_add_flash_card()
         elif user_choice == '2':
-            menu_practice_flash_card()
+            if session.query(Flashcard).all():
+                menu_practice_flash_card()
+            else:
+                print("There is no flashcard to practice!")
         elif user_choice == '3':
             print('Bye!')
             exit()
